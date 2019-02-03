966d1989fce5d5801c34d982c058c0822694b3c5
lmartinez63 <l.martinez.rod@gmail.com>
2019-02-02 19:00:55 -0500

    Improve getpatinetbydocumentnumber

--- hcare_backend/src/main/java/com/landl/hcare/component/CustomProcessSelector.java
+++ hcare_backend/src/main/java/com/landl/hcare/component/CustomProcessSelector.java
@@ -9,11 +9,17 @@ public class CustomProcessSelector {
     @Autowired
     RetrieveMedicalAppointmentInfo retrieveMedicalAppointmentInfo;
 
+    @Autowired
+    GetPatientByDocumentNumber getPatientByDocumentNumber;
+
     public CustomProcess getCustomProcessClass(String customProcessName) throws Exception{
         CustomProcess customProcess = null;
         switch(customProcessName){
             case "RetrieveMedicalAppointmentInfo":
                 customProcess = retrieveMedicalAppointmentInfo;
+                break;
+            case "GetPatientByDocumentNumber":
+                customProcess = getPatientByDocumentNumber;
                 break;
             default:
                     throw new Exception("Process "+customProcessName+"has not been found");
